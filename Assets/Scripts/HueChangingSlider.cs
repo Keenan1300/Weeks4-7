acbf651e8333f87217ed56dab21e1ecf3cb835de
hamilt25 <hamilt25@OA-S138-15>
2025-02-20 10:49:57 -0500

    De-Cluttercode in huechanging slider

--- Assets/Scripts/HueChangingSlider.cs
+++ Assets/Scripts/HueChangingSlider.cs
@@ -6,14 +6,8 @@ using UnityEngine.UI;
 
 public class HueChangingSlider : MonoBehaviour
 {
-    private SpriteRenderer SpriteRenderer;
-    Color slidingcolor;
-    float Hue;
     Renderer ColorRenderer;
-    public Slider Slider;
-    float Sat;
-    float Val;
-
+ 
     void Start()
     {
         ColorRenderer = GetComponent<Renderer>();
@@ -23,7 +17,6 @@ public class HueChangingSlider : MonoBehaviour
     // Update is called once per frame
     void Update()
    {
-        Hue = Slider.value;
     }
 
     //Function changes color thats applied to character sprite
