57cf383a1086d76418b7cde6afc728e6075cb7d0
hamilt25 <hamilt25@OA-S143-09>
2025-02-20 23:33:06 -0500

    Tweaked Hat system

--- Assets/Scripts/HatRandomGenerator.cs
+++ Assets/Scripts/HatRandomGenerator.cs
@@ -2,64 +2,114 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UIElements;
+using static Unity.Burst.Intrinsics.X86.Avx;
 
 public class HatRandomGenerator : MonoBehaviour
 {
     //Introduce Chance integer
-    int ChanceHat;
-    
-    //Find and collect game hat objects so systems recognizes them
-    GameObject hat = GameObject.Find("90shat");
-    GameObject beanie = GameObject.Find("beanie");
-    GameObject cap = GameObject.Find("Cap");
-    GameObject wrap = GameObject.Find("Headwrap");
+    float ChanceHat;
 
+    public GameObject beanie;
+    public GameObject wrap;
+    public GameObject oldhat;
+    public GameObject cap;
 
+    public Transform SpawnLocation;
 
+    bool Hatison;
+    bool Wrapison;
+    bool beanieison;
+    bool capison;
 
     // Start is called before the first frame update
     void Start()
     {
-        //Ensure all hats are invisible at the start of the game. None have been selected yet!
-        hat.SetActive(false);
-        wrap.SetActive(false);
-        beanie.SetActive(false);
-        cap.SetActive(false);
+        Hatison = false;
+        Wrapison = false;
+        beanieison = false;
+        capison = false;
+
+        beanie = Resources.Load("beanie") as GameObject;
+
     }
 
     // Update is called once per frame
     void Update()
     {
+        //Find and collect game hat objects so systems recognizes them
+        GameObject hat = GameObject.Find("Detecthat");
+        GameObject beanie = GameObject.Find("beanie");
+        GameObject cap = GameObject.Find("Cap");
+        GameObject wrap = GameObject.Find("Headwrap");
+
+        hat = Resources.Load("Detecthat") as GameObject;
+        beanie = Resources.Load("beanie") as GameObject;
+        cap = Resources.Load("Cap") as GameObject;
+        wrap = Resources.Load("Headwrap") as GameObject;
+
+        if (Hatison == true)
+        {
+            Instantiate(oldhat, SpawnLocation.position, Quaternion.identity);
+        }
       
+
+        if (Wrapison == true)
+        {
+            Instantiate(wrap, SpawnLocation.position, Quaternion.identity);
+        }
+      
+
+        if (beanieison == true)
+        {
+            Instantiate(beanie, SpawnLocation.position, Quaternion.identity);
+        }
+       
+
+        if (capison == true)
+        {
+            Instantiate(cap, SpawnLocation.position, Quaternion.identity);
+        }
+       
+
+
     }
 
 
     public void Generate()
     {
-        ChanceHat = Random.Range(1, 4);
+        int ChanceHat = Random.Range(1, 4);
+        Debug.Log(ChanceHat);
 
-        if (ChanceHat == 4) 
+        if (ChanceHat == 4)
         {
-            GameObject hat = GameObject.Find("90shat");
-            hat.SetActive(true);
+            Wrapison = true;
+            capison = false;
+            Hatison = false;
+            beanieison = false;
         }
 
         if (ChanceHat == 3)
         {
-            GameObject beanie = GameObject.Find("beanie");
-            beanie.SetActive(true);
+            capison = true;
+            Wrapison = false;
+            Hatison = false;
+            beanieison = false;
+
         }
         if (ChanceHat == 2)
         {
-            GameObject cap = GameObject.Find("Cap");
-            cap.SetActive(true);
+            beanieison = true;
+            capison = false;
+            Wrapison = false;
+            Hatison = false;
         }
         if (ChanceHat == 1)
         {
-            GameObject wrap = GameObject.Find("Headwrap");
-            wrap.SetActive(true);
+            Hatison = true;
+            capison = false;
+            Wrapison = false;
+            beanieison = false;
         }
-
     }
 
 
